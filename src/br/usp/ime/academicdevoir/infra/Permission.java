36c924e26fb3e6ef6f39dd57e48125d68a296d4f
Wallace <wallace.almeida89@gmail.com>
2011-09-28 10:07:34 -0300

    Readicionando os arquivos de permissao e privilegio

---
+++ src/br/usp/ime/academicdevoir/infra/Permission.java
@@ -0,0 +1,15 @@
+package br.usp.ime.academicdevoir.infra;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+public @interface Permission {
+
+    Privilegio[] value();
+
+}
+
---
+++ src/br/usp/ime/academicdevoir/infra/Privilegio.java
@@ -0,0 +1,5 @@
+package br.usp.ime.academicdevoir.infra;
+
+public enum Privilegio {
+    ALUNO, MONITOR, PROFESSOR, ADMINISTRADOR;
+}
